8d411fcc5fff39c0bf3a48abb95a65916a25a625
Manuel <manuel_jose_2002@hotmail.com>
2022-10-27 04:09:29 +0100

    TP06 incompleta
    penultimo por fazer
    ultimo a funcionar para só alguns casos

--- LP1_2UFP/main.c
+++ LP1_2UFP/main.c
@@ -20,14 +20,14 @@ int main(int argc, const char* argv[]) {
     //main_tp03(argc, argv);
     //main_tp04(argc, argv);
     //main_tp05(argc, argv);
-    //main_tp06(argc, argv);
+    main_tp06(argc, argv);
     //main_pr01(argc, argv);
     //main_pr02(argc, argv);
     //main_pr03(argc, argv);
     //main_pr04(argc, argv);
     //main_pr05(argc, argv);
 
-    main_testes(argc, argv);
+    //main_testes(argc, argv);
 
     return 0;
 }
--- LP1_2UFP/tp/tp06.c
+++ LP1_2UFP/tp/tp06.c
@@ -7,12 +7,20 @@
 int main_tp06(int argc, const char * argv[]){
 
     const char *str[] = {"254", "123", "534", "123"};
-    int v[] = {};
+    const char *frases[] = {"um", "quatro", "nove"};
+    const char *strRep[] = {"aa", "bbb", "ccc", "aa", "ccc", "ddd"};
+    char strings[][MAX128] = {""};
+    char consonants[][MAX128] = {""};
+    int v[MAX128] = {};
     int sizev = 0;
+    int maxmin[MAX128] = {};
 
     //print_arraystrs(argc, argv);
     //printf("%p\n", convert_str2int_arraystrs(2, str, v));
-    printf("%p\n", remove_repeated_ints(4, str, v, &sizev));
+    //printf("%p\n", remove_repeated_ints(4, str, v, &sizev));
+    //printf("%p\n", get_arrayint_maxmin(4,str,maxmin));
+    //count_consonants(3, frases, strings, consonants);
+    print_non_repeated_strs(strRep, 6);
 
     return 0;
 }
@@ -36,6 +44,41 @@ int* convert_str2int_arraystrs(int size, const char *astr[], int v[]){
     return v;
 }
 
+int* remove_repeated_ints(int c, const char *astr[], int v[], int *psizev){
+
+    *psizev = c;
+
+    convert_str2int_arraystrs(c, astr, v);
+
+    for (int i = 0; i < *psizev; ++i) {
+        if(exists_before(v,i) != -1){
+            move_vector_content_oneback(v,i, psizev);
+            /*
+            for (int j = i; j < *psizev; j++) {
+                v[j] = v[j+1];
+            }
+            *(psizev) -= 1;
+            i--;
+             */
+        }
+    }
+
+    for (int i = 0; i < *psizev; ++i) {
+        printf("%d\n", v[i]);
+    }
+
+    //printf("%d\n", *psizev);
+    return v;
+}
+
+int* move_vector_content_oneback(int v[], int i, int *psize){
+    for (int j = i; j < *psize; j++) {
+        v[j] = v[j+1];
+    }
+    *(psize) -= 1;
+    i--;
+}
+
 int exists_before(int v[], int i){
     for (int j = 0; j < i; j++) {
         if(v[i] == v[j]){
@@ -45,22 +88,77 @@ int exists_before(int v[], int i){
     return -1;
 }
 
-int* remove_repeated_ints(int c, const char *astr[], int v[], int *psizev){
+int* get_arrayint_maxmin(int c, const char *astr[], int maxmin[]){
 
-    *psizev = c;
+    int v[MAX128] = {};
+    int vSize = c;
 
-    convert_str2int_arraystrs(c, astr, v);
+    convert_str2int_arraystrs(c,astr,v);
 
-    for (int i = 0; i < *psizev; ++i) {
-        if(exists_before(v,i) != -1){
-            printf("%d\n", v[i]);
-            for (int j = i; j < *psizev; j++) {
-                v[j] = v[j+1];
-            }
-            *(psizev)--;
+    maxmin[0] = 0;
+    maxmin[1] = v[0];
+
+    for (int i = 0; i < vSize; ++i) {
+        if(v[i] > maxmin[0]){
+            maxmin[0] = v[i];
+        } else if (v[i] < maxmin[0]){
+            maxmin[1] = v[i];
         }
     }
 
-    printf("%d\n", *psizev);
-    return v;
+    for (int i = 0; i < 2; ++i) {
+        printf("%d\n", maxmin[i]);
+    }
+
+    return maxmin;
+}
+
+void count_consonants(int c, const char *astr[], char strings[][MAX128], char consonants[][MAX128]){
+
+    int j = 0;
+    char count[] = {48}, t='0';
+
+    for (int i = 0; i < c; ++i) {
+        strcpy(strings[i], astr[i]);
+        while (astr[i][j] != '\0'){
+            if(isalpha(astr[i][j])){
+                if(is_consonant(astr[i][j])){
+                    printf("%c\n", count[i]);
+                    t += '1';
+                    count[i] = t;
+                    strcpy(consonants[i], strings[i]);
+                }
+            }
+            j++;
+        }
+        t='0';
+        j=0;
+    }
+
+    for (int i = 0; i < c; ++i) {
+        printf("%s\n", consonants[i]);
+    }
+}
+
+void print_non_repeated_strs(const char *astr[], int size){
+
+    int j = 0, find = 0;
+
+    for (int i = 0; i < size; ++i) {
+        for (int k = i+1; k < size; ++k) {
+            if(astr[i] == astr[k]){
+                astr[k] = astr[k+1];
+                find = 1;
+            }
+        }
+        if(find == 1){
+            size--;
+            i--;
+            find=0;
+        }
+    }
+
+    for (int i = 0; i < size+1; ++i) {
+        printf("%s\n", astr[i]);
+    }
 }
