e26f4e925f5402e96ef38126dbf5375966370546
Manuel <84775761+Solixx@users.noreply.github.com>
2022-11-11 22:10:55 +0000

    Update main.c

--- AED1_2UFP/main.c
+++ AED1_2UFP/main.c
@@ -4,7 +4,6 @@
 #include "aed1_03.h"
 #include "aed1_04.h"
 #include "aed1_05.h"
-#include "aed1_06.h"
 
 #include "teste_estudo/teste_estudo.h"
 
@@ -17,7 +16,6 @@ int main() {
     //main_pr_aed1_03();
     //main_pr_aed1_04();
     //main_pr_aed1_05();
-    main_pr_aed1_06();
 
     //main_pr_aed1_teste1();
     //main_pr_aed1_teste2();
