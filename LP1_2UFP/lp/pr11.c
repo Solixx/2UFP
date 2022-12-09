9381d5f55a18799efd0a46889a06fb0d5735820e
Manuel <84775761+Solixx@users.noreply.github.com>
2022-12-09 19:22:10 +0000

    LP pr11

--- LP1_2UFP/lp/pr11.c
+++ LP1_2UFP/lp/pr11.c
@@ -6,5 +6,48 @@
 
 int main_pr11(int argc, const char * argv[]){
 
+    GAME_DYNARPLAYERS game;
+    const char *nomes[] = {"Manuel", "Jose"};
+    char player[] = "T";
+
+    game = create_array_players(nomes, 3);
+
+    for (int i = 0; i < game.nplayers; ++i) {
+        printf("Jogador%d: %s\n", i, game.pplayer[i].pusername);
+    }
+
+    insert_player_into_dynarray(&game, player);
+
+    for (int i = 0; i < game.nplayers; ++i) {
+        printf("Jogador%d: %s\n", i, game.pplayer[i].pusername);
+    }
+
     return 0;
+}
+
+GAME_DYNARPLAYERS create_array_players(const char *pnames[], int size){
+
+    GAME_DYNARPLAYERS *game = (GAME_DYNARPLAYERS*) calloc(1, sizeof (GAME_DYNARPLAYERS));
+    game->nplayers = size;
+
+    game->pplayer = (PLAYER_LLCARDS*) calloc(size, sizeof (PLAYER_LLCARDS));
+
+    for (int i = 0; i < game->nplayers; ++i) {
+        game->pplayer[i].pusername = (char*) calloc(strlen(pnames[i]), sizeof (char));
+        strcpy(game->pplayer[i].pusername, pnames[i]);
+        game->pplayer[i].pcard = NULL;
+    }
+    return *game;
+}
+
+void insert_player_into_dynarray(GAME_DYNARPLAYERS *pg, char player[]){
+
+    PLAYER_LLCARDS empty = {NULL, NULL};
+    for (int i = 0; i < pg->nplayers; ++i) {
+        if(pg->pplayer[i] == empty){
+            pg->pplayer[i].pusername = (char*) calloc(strlen(player), sizeof (char));
+            strcpy(pg->pplayer[i].pusername, player);
+            break;
+        }
+    }
 }
